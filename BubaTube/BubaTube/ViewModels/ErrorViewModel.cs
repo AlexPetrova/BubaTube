ccafe9ac62a1e675065cd63631854e9ea6ca6dd6
AlexPetrova <a.petrova95@yahoo.com>
2018-05-17 23:45:38 +0300

    ASP.NET Core Web App

---
+++ BubaTube/BubaTube/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BubaTube.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
