945acf3c1c5819fb9d0ada4e6608f95fe3eea2e9
AlexPetrova <a.petrova95@yahoo.com>
2018-08-13 21:05:47 +0300

    JSON builder

---
+++ BubaTube/BubaTube/Helpers/JSON/JSONObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubaTube.Helpers.JSON
+{
+    public class JSONObject
+    {
+        private StringBuilder content;
+
+        public JSONObject(StringBuilder content)
+        {
+            this.content = content;
+        }
+
+        public void AddProperty(string name, string value)
+        {
+            this.content.AppendFormat(@"{""{0}"": ""{1}""}", name, value);
+        }
+        
+        public override string ToString()
+        {
+            return this.content.ToString(); 
+        }
+    }
+}
