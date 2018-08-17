44308af8c4c516ea41f7c0e66c20385af14d3afc
AlexPetrova <a.petrova95@yahoo.com>
2018-08-17 22:42:45 +0300

    added constants

---
+++ BubaTube/BubaTube/Helpers/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace BubaTube.Helpers.Constants
+{
+    public static class Constants
+    {
+        public const string VideosQuery = "SELECT * FROM Videos";
+        public const string CommentsQuery = "SELECT * FROM Comments";
+        public const string UsersQery = "SELECT * FROM Users";
+        public const string VideosTable = "Videos";
+        public const string UsersTable = "Users";
+        public const string CommentsTable = "Comments";
+    }
+}
