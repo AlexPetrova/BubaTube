c56ae943bf974051928166942b5d7d871c01bfeb
AlexPetova <a.petrova95@yahoo.com>
2018-09-15 14:02:04 +0300

    moved logic for generating path for video to UploadVideoHelper

---
+++ BubaTube/BubaTube/Helpers/Contracts/IUploadVideoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BubaTube.Helpers.Contracts
+{
+    public interface IUploadVideoHelper
+    {
+        /// <summary>
+        /// Generates path in wwwrooth folder using GUID for name of the video file.
+        /// </summary>
+        /// <returns>Path in wwwroot folder</returns>
+        string GeneratePath(string environmetWebRootFolder);
+    }
+}
---
+++ BubaTube/BubaTube/Helpers/UploadVideoHelper.cs
@@ -0,0 +1,21 @@
+﻿using BubaTube.Helpers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BubaTube.Helpers
+{
+    public class UploadVideoHelper : IUploadVideoHelper
+    {
+        public string GeneratePath(string environmetWebRootFolder)
+        {
+            var folder = Path.Combine(environmetWebRootFolder, "video");
+            var nameOfVideo = Guid.NewGuid();
+            var path = Path.Combine(folder, nameOfVideo.ToString() + ".mp4");
+
+            return path;
+        }
+    }
+}
