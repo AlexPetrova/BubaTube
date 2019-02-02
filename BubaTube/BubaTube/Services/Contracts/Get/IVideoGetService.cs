29b92f1df8d8db15610924aa8b122b87e7cdf8c5
AlexPetrova <a.petrova95@yahoo.com>
2019-02-02 16:15:15 +0200

    service for building home page

---
+++ BubaTube/BubaTube/Services/Contracts/Get/IVideoGetService.cs
@@ -0,0 +1,18 @@
+﻿using BubaTube.Data.DTO;
+using BubaTube.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BubaTube.Services.Contracts.Get
+{
+    public interface IVideoGetService
+    {
+        IEnumerable<VideoDTO> MostResentVideos();
+
+        IEnumerable<VideoDTO> PopularVideos();
+
+        IEnumerable<VideoDTO> UserMayLikeVideos(User user);
+    }
+}
---
+++ BubaTube/BubaTube/Services/GetServices/VideoGetService.cs
@@ -0,0 +1,49 @@
+﻿using BubaTube.Data;
+using BubaTube.Data.DTO;
+using BubaTube.Data.Models;
+using BubaTube.Services.Contracts.Get;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubaTube.Services.GetServices
+{
+    public class VideoGetService : IVideoGetService
+    {
+        private BubaTubeDbContext context;
+        private const int DefaultCountForResentVideos = 20;
+
+        public VideoGetService(BubaTubeDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<VideoDTO> MostResentVideos()
+        {
+            var resentVideos = this.context.Videos.
+                Where(x => x.IsАpproved == true).
+                Take(DefaultCountForResentVideos).
+                Select(x => 
+                    new VideoDTO()
+                    {
+                        Title = x.Title,
+                        Path = x.Path,
+                        Likes = x.Likes,
+                        AuthorId = x.AuthorId
+                    });
+
+            return resentVideos.ToList();
+        }
+
+        public IEnumerable<VideoDTO> PopularVideos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<VideoDTO> UserMayLikeVideos(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
