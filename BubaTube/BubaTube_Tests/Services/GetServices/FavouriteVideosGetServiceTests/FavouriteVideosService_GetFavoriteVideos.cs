f1e16d39e4df817f548fa32bbae3a1039e5051d9
AlexPetova <a.petrova95@yahoo.com>
2019-04-13 22:11:14 +0300

    FavouriteVideosGetService test

--- BubaTube/BubaTube_Tests/Services/GetServices/FavouriteVideosGetServiceTests/FavouriteVideosService_GetFavoriteVideos.cs
+++ BubaTube/BubaTube_Tests/Services/GetServices/FavouriteVideosGetServiceTests/FavouriteVideosService_GetFavoriteVideos.cs
@@ -51,6 +51,32 @@ namespace BubaTube_Tests.Services.GetServices.FavouriteVideosGetServiceTest
             }
         }
 
+        [Fact]
+        public void ReturnsEmptyListIfNoFavouriteVideosSaved()
+        {
+            var options = this.GetOptions("GetFavouriteVideosTest");
+            using (var context = new BubaTubeDbContext(options))
+            {
+                context.Users.Add(new User()
+                {
+                    UserName = "testUser"
+                });
+                context.SaveChanges();
+
+                var user = context.Users.First();
+
+                var favoutiteVideoService = new FavouriteVideosGetService(context);
+                var userDto = new UserSearchDTO()
+                {
+                    Id = user.Id
+                };
+
+                var result = favoutiteVideoService.GetFavouriteVideos(userDto);
+
+                Assert.Empty(result);
+            }
+        }
+
         private DbContextOptions<BubaTubeDbContext> GetOptions(string name)
         {
             return new DbContextOptionsBuilder<BubaTubeDbContext>()
