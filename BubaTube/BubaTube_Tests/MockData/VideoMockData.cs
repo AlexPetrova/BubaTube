cf81a7906c471baa6262904032e184f5baf87113
AlexPetova <a.petrova95@yahoo.com>
2019-04-13 22:44:34 +0300

    class for generating mock video objects

---
+++ BubaTube/BubaTube_Tests/MockData/VideoMockData.cs
@@ -0,0 +1,31 @@
+﻿using BubaTube.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubaTube_Tests.MockData
+{
+    public static class VideoMockData
+    {
+        public static IEnumerable<Video> GetVideos(int count)
+        {
+            var videos = new List<Video>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var video = new Video()
+                {
+                    Id = random.Next(),
+                    Title = RandomString(5),
+                    Description = RandomString(10),
+                    Path = RandomString(10),
+                    AuthorId = RandomString(10)
+                };
+
+                videos.Add(video);
+            }
+
+            return videos;
+        }
+    }
+}
