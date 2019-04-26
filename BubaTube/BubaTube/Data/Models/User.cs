151bb3f8296ac49a5fae3f0aaec56a78e522e40f
AlexPetrova <a.petrova95@yahoo.com>
2019-04-26 20:20:04 +0300

    remove comment in User.cs

--- BubaTube/BubaTube/Data/Models/User.cs
+++ BubaTube/BubaTube/Data/Models/User.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BubaTube.Data.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class User : IdentityUser
     {
         [Required]
