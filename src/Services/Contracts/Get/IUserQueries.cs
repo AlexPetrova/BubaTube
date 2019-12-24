a76a5277f22e5482a3e8ecd6b945708493a89732
AlexPetova <a.petrova95@yahoo.com>
2019-12-24 22:47:42 +0200

    remove SearchUsetsService from admin area
    delete methods which are getting the user with the usermanager - the only logic in those methods was the mapping of the User object to UserDTO object which can be done in the Controller
    create UserQueries

---
+++ src/Services/Contracts/Get/IUserQueries.cs
@@ -0,0 +1,15 @@
+﻿using Contracts.Data.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace Services.Contracts.Get
+{
+    public interface IUserQueries
+    {
+        IReadOnlyCollection<UserDTO> RegisterdInPeriod(DateTime fromDate, DateTime toDate);
+
+        IReadOnlyCollection<UserDTO> RegisteredAfter(DateTime date);
+
+        IReadOnlyCollection<UserDTO> ByLastActivity(int months);
+    }
+}
---
+++ src/Services/Get/UserQueries.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.Data.DTO;
+using Contracts.Data.Models;
+using DataAccess;
+using Services.Contracts.Get;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services.Get
+{
+    public class UserQueries : IUserQueries
+    {
+        private readonly BubaTubeDbContext context;
+        private readonly Func<User, UserDTO> userMapper;
+
+        public UserQueries(
+            BubaTubeDbContext context,
+            Func<User, UserDTO> userMapper)
+        {
+            this.context = context;
+            this.userMapper = userMapper;
+        }
+
+        public IReadOnlyCollection<UserDTO> RegisterdInPeriod(DateTime fromDate, DateTime toDate)
+        {
+            return this.context.Users
+                .Where(x => x.RegisteredOn > fromDate && x.RegisteredOn < toDate)
+                .Select(this.userMapper)
+                .ToList();
+        }
+
+        public IReadOnlyCollection<UserDTO> RegisteredAfter(DateTime date)
+        {
+            return this.context.Users
+                   .Where(x => x.RegisteredOn > date)
+                   .Select(this.userMapper)
+                   .ToList();
+        }
+
+
+        public IReadOnlyCollection<UserDTO> ByLastActivity(int months)
+        {
+            var startingTime = DateTime.Now.AddMonths(months * -1);
+            return this.context.Users
+                .Where(x => x.LastLogin >= startingTime)
+                .Select(this.userMapper)
+                .ToList();
+        }
+    }
+}
