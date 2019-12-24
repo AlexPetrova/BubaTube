a429f78e964b861b42c0865948f88eaeb979c63b
AlexPetova <a.petrova95@yahoo.com>
2019-12-24 17:47:32 +0200

    add DataAccess project

---
+++ src/DataAccess/Migrations/20190426174759_changeNameOfColUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BubaTube.Migrations
+{
+    public partial class changeNameOfColUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfRegistering",
+                table: "AspNetUsers",
+                newName: "RegisteredOn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegisteredOn",
+                table: "AspNetUsers",
+                newName: "DateOfRegistering");
+        }
+    }
+}
---
+++ src/DataAccess/Migrations/20190426174953_requireDateOfRegisterAndLastLogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BubaTube.Migrations
+{
+    public partial class requireDateOfRegisterAndLastLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
