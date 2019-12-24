de92942fa7048a52cf25cfd7a9fd705157c148dc
AlexPetova <a.petrova95@yahoo.com>
2019-12-24 22:34:08 +0200

    add ServicesRegistrationExtensions in DataAccess project

---
+++ src/DataAccess/Extensions/ServicesRegistrationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Contracts.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DataAccess.Extensions
+{
+    public static class ServicesRegistrationExtensions
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction = null)
+        {
+            services.AddDbContext<BubaTubeDbContext>(optionsAction);
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<BubaTubeDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
