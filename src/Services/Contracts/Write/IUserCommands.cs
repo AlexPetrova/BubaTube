5434620de9e4e02e337ab08a52016aec981f9399
AlexPetova <a.petrova95@yahoo.com>
2019-12-24 22:02:54 +0200

    remove ManageUsersService and move the CloseAccount method to UserCommands class

--- src/Services/Contracts/Write/IUserCommands.cs
+++ src/Services/Contracts/Write/IUserCommands.cs
@@ -5,5 +5,7 @@ namespace Services.Contracts.Write
     public interface IUserCommands
     {
         Task<int> SaveLoginDate(string userID);
+
+        Task<int> CloseAccount(string userEmail);
     }
 }
--- src/Services/Write/UserCommands.cs
+++ src/Services/Write/UserCommands.cs
@@ -1,4 +1,7 @@
-﻿using DataAccess;
+﻿using Contracts.Data.Models;
+using DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Services.Contracts.Write;
 using System;
 using System.Linq;
@@ -8,11 +11,15 @@ namespace Services.Write
 {
     public class UserCommands : IUserCommands
     {
-        private BubaTubeDbContext context;
+        private readonly BubaTubeDbContext context;
+        private readonly UserManager<User> userManager;
 
-        public UserCommands(BubaTubeDbContext context)
+        public UserCommands(
+            BubaTubeDbContext context,
+            UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
         }
 
         public async Task<int> SaveLoginDate(string userID)
@@ -23,6 +30,13 @@ namespace Services.Write
             var result = await context.SaveChangesAsync();
 
             return result;
+        }
+
+        public async Task<int> CloseAccount(string userEmail)
+        {
+            var user = await this.userManager.FindByEmailAsync(userEmail);
+            this.context.Entry(user).State = EntityState.Deleted;
+            return await this.context.SaveChangesAsync();
         }
     }
 }
