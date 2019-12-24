13da32dabef627176634d82bf2adcba8eaaeae26
AlexPetova <a.petrova95@yahoo.com>
2019-12-24 16:41:45 +0200

    move solution to src folder

---
+++ src/BubaTube_Tests/Services/GetServices/VideoGetServiceTests/VideoGetService_MostResentVideos.cs
@@ -0,0 +1,16 @@
+﻿using BubaTube_Tests.MockData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BubaTube_Tests.Services.GetServices.VideoGetServiceTests
+{
+    public class VideoGetService_MostResentVideos
+    {
+        [Fact]
+        public void ReturnListOfMostResentVideos()
+        {
+        }
+    }
+}
---
+++ src/BubaTube_Tests/Services/WriteServices/VideoWriteServiceTests/VideoWriteService_SaveToRootFolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubaTube_Tests.Services.WriteServices.VideoWriteServiceTests
+{
+    public class VideoWriteService_SaveToRootFolder
+    {
+
+
+    }
+}
