96b4d3024edfc6ec37b778d201f214435817640d
AlexPetrova <a.petrova95@yahoo.com>
2020-03-30 18:55:58 +0300

    add filter for last activity of users in admin area

--- src/BubaTube/Areas/Admin/Controllers/UsersController.cs
+++ src/BubaTube/Areas/Admin/Controllers/UsersController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Contracts.Data.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts.Get;
+using System.Collections.Generic;
 
 namespace BubaTube.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("[area]/[controller]/[action]")]
     public class UsersController : Controller
     {
         private IUserQueries userQueries;
@@ -14,11 +17,20 @@ namespace BubaTube.Areas.Admin.Controllers
             this.userQueries = userQueries;
         }
 
-        [HttpGet]
         [Authorize(Roles = "Admin")]
+        [HttpGet]
         public IActionResult ManageUsers()
         {
             return base.View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{months}")]
+        public IActionResult Filter(int months)
+        {
+            IReadOnlyCollection<UserDTO> users = this.userQueries.ByLastActivity(months);
+            
+            return base.Ok(users);
         }
     }
 }
---
+++ src/BubaTube/wwwroot/js/admin-area.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $('#user-activity-filter-button').on('click', function (event) {
+        var url = 'filter/' + $('.drop-down-element.active').first().data("months");
+        
+        $.get(
+            url,
+            function (data) {
+                console.log(data);
+            }
+        );
+    });
+
+    $('.drop-down-element').click(function (e) {
+        $('.drop-down-element.active')
+            .toArray()
+            .forEach(element => element.classList.remove('active'));
+
+        this.classList.add('active');
+
+        $('#user-activity-filter-period-text').first().text(this.textContent.trim())
+    });
+});
