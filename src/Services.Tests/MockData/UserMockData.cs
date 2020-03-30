48bc4bcd483e341a4d7a52d0e93f9ecda2a7a43a
AlexPetrova <a.petrova95@yahoo.com>
2020-03-30 22:17:51 +0300

    add unit test for UsersQueries - filter by activity

---
+++ src/Services.Tests/MockData/UserMockData.cs
@@ -0,0 +1,71 @@
+﻿using Contracts.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Tests.MockData
+{
+    internal class UserMockData
+    {
+        internal static IReadOnlyCollection<User> GetListOfUserModels()
+        {
+            return new List<User>
+            {
+                new User
+                {
+                    FirstName = "First_Name_01",
+                    LastName = "Last_Name_01",
+                    Email = "First_Mail_01@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -1).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -1),
+                    AvatarImage = new byte[1]
+                },
+                new User
+                {
+                    FirstName = "First_Name_02",
+                    LastName = "Last_Name_02",
+                    Email = "First_Mail_02@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -3).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -3),
+                    AvatarImage = new byte[1]
+                },
+                new User
+                {
+                    FirstName = "First_Name_03",
+                    LastName = "Last_Name_03",
+                    Email = "First_Mail_03@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -3).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -3),
+                    AvatarImage = new byte[1]
+                },
+                new User
+                {
+                    FirstName = "First_Name_04",
+                    LastName = "Last_Name_04",
+                    Email = "First_Mail_04@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -4).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -4),
+                    AvatarImage = new byte[1]
+                },
+                new User
+                {
+                    FirstName = "First_Name_05",
+                    LastName = "Last_Name_05",
+                    Email = "First_Mail_05@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -4).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -4),
+                    AvatarImage = new byte[1]
+                },
+                new User
+                {
+                    FirstName = "First_Name_06",
+                    LastName = "Last_Name_06",
+                    Email = "First_Mail_06@abv.bg",
+                    LastLogin = DateTime.Now.AddMonths(1 * -4).AddHours(1),
+                    RegisteredOn = DateTime.Now.AddMonths(1 * -4),
+                    AvatarImage = new byte[1]
+                }
+            };
+        }
+    }
+}
---
+++ src/Services.Tests/UserQueries_Should.cs
@@ -0,0 +1,41 @@
+﻿using Contracts.Data.DTO;
+using Contracts.Data.Models;
+using DataAccess;
+using Services.Get;
+using Services.Tests.MockData;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Services.Tests
+{
+    public class UserQueries_Should
+    {
+        static readonly Func<User, UserDTO> fakeMapper = new Func<User, UserDTO>(_ => new UserDTO());
+
+        [Fact]
+        public void ReturnsUsers_LastActivity()
+        {
+            var options = DbContextMock.GetOptions("ReturnsUsers_LastActivity");
+            IReadOnlyCollection<UserDTO> lastOneMonth;
+            IReadOnlyCollection<UserDTO> lastThreeMonth;
+            IReadOnlyCollection<UserDTO> lastFourMonth;
+
+            using (var context = new BubaTubeDbContext(options))
+            {
+                context.AddRange(UserMockData.GetListOfUserModels());
+                context.SaveChanges();
+
+                var userQueries = new UserQueries(context, fakeMapper);
+
+                lastOneMonth = userQueries.ByLastActivity(1);
+                lastThreeMonth = userQueries.ByLastActivity(3);
+                lastFourMonth = userQueries.ByLastActivity(4);
+            }
+
+            Assert.Equal(1, lastOneMonth.Count);
+            Assert.Equal(3, lastThreeMonth.Count);
+            Assert.Equal(6, lastFourMonth.Count);
+        }
+    }
+}
