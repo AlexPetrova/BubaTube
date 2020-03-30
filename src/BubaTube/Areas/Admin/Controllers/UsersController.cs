1a7e7cc2418dd681e9047218f4099f34de82d866
AlexPetrova <a.petrova95@yahoo.com>
2020-03-30 22:13:10 +0300

    add authorize for admin are attribute

--- src/BubaTube/Areas/Admin/Controllers/UsersController.cs
+++ src/BubaTube/Areas/Admin/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 namespace BubaTube.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     [Route("[area]/[controller]/[action]")]
     public class UsersController : Controller
     {
@@ -17,20 +18,24 @@ namespace BubaTube.Areas.Admin.Controllers
             this.userQueries = userQueries;
         }
 
-        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult ManageUsers()
         {
             return base.View();
         }
 
-        [Authorize(Roles = "Admin")]
         [HttpGet("{months}")]
-        public IActionResult Filter(int months)
+        public IActionResult FilterByLastActivity(int months)
         {
             IReadOnlyCollection<UserDTO> users = this.userQueries.ByLastActivity(months);
-            
-            return base.Ok(users);
+           
+            return base.PartialView("_FilterResult", users);
+        }
+
+        [HttpGet]
+        public IActionResult ActivityFilter()
+        {
+            return this.View();
         }
     }
 }
