7df916d8ea4c0010c08c460bc01f17508326b158
AlexPetrova <a.petrova95@yahoo.com>
2020-04-01 17:15:03 +0300

    add routs for partial filter views

--- src/BubaTube/Areas/Admin/Controllers/UsersController.cs
+++ src/BubaTube/Areas/Admin/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts.Get;
+using System;
 using System.Collections.Generic;
 
 namespace BubaTube.Areas.Admin.Controllers
@@ -24,18 +25,32 @@ namespace BubaTube.Areas.Admin.Controllers
             return base.View();
         }
 
+        [HttpGet]
+        public IActionResult FilterByLastActivity()
+        {
+            return base.PartialView("_FilterByLastActivity");
+        }
+
+        [HttpGet]
+        public IActionResult FilterByPeriod()
+        {
+            return base.PartialView("_FilterByPeriod");
+        }
+
         [HttpGet("{months}")]
         public IActionResult FilterByLastActivity(int months)
         {
             IReadOnlyCollection<UserDTO> users = this.userQueries.ByLastActivity(months);
-           
+
             return base.PartialView("_FilterResult", users);
         }
 
-        [HttpGet]
-        public IActionResult ActivityFilter()
+        [HttpGet("{from}/{to}")]
+        public IActionResult FilterPerPeriod(DateTime from, DateTime to)
         {
-            return this.View();
+            IReadOnlyCollection<UserDTO> users = this.userQueries.RegisterdInPeriod(from, to);
+
+            return base.PartialView("_FilterResult", users);
         }
     }
 }
