958fb530b0c0589591822a8b3db4255a64045c58
AlexPetrova <a.petrova95@yahoo.com>
2020-04-10 14:43:27 +0300

    add Video model

---
+++ src/BubaTube/Areas/Admin/Models/Video.cs
@@ -0,0 +1,13 @@
+﻿namespace BubaTube.Areas.Admin.Models
+{
+    public class Video
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Path { get; set; }
+
+        public double Likes { get; set; }
+    }
+}
