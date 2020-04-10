ce5a0273c5b5b37e99b5c1d545b3e0511e5571b1
AlexPetrova <a.petrova95@yahoo.com>
2020-04-10 14:42:44 +0300

    add video controller, view and tab

---
+++ src/BubaTube/Areas/Admin/Controllers/VideosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts.Get;
+
+namespace BubaTube.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class VideosController : Controller
+    {
+        private IVideoQueries videoQueries;
+
+        public VideosController(IVideoQueries videoQueries)
+        {
+            this.videoQueries = videoQueries;
+        }
+
+        [HttpGet]
+        public IActionResult ManageVideos()
+        {
+            return View();
+        }
+    }
+}
