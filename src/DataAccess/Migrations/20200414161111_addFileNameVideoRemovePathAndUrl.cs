22566aec0d73d850316efb8f31aa64eba7cb6d86
AlexPetrova <a.petrova95@yahoo.com>
2020-04-14 19:24:07 +0300

    delete column Url and Path from Video
    add column FileName

---
+++ src/DataAccess/Migrations/20200414161111_addFileNameVideoRemovePathAndUrl.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BubaTube.Migrations
+{
+    public partial class addFileNameVideoRemovePathAndUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "Videos");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Videos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "Videos",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "Videos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "Videos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Videos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
