7df3521aa87504f8c5eeb5795899ae3ca5168576
AlexPetrova <a.petrova95@yahoo.com>
2020-04-14 19:22:11 +0300

    add FileQueries class

---
+++ src/Services/Contracts/Get/IFileQueries.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Contracts.Get
+{
+    public interface IFileQueries
+    {
+        PathInfo GenerateVideoPath(string fileExtension);
+
+        string GetVideoPath(string fileName);
+    }
+}
---
+++ src/Services/Get/FileQueries.cs
@@ -0,0 +1,35 @@
+﻿using Services.Contracts;
+using Services.Contracts.Get;
+using Services.Extensions;
+using System;
+using System.IO;
+
+namespace Services.Get
+{
+    public class FileQueries : IFileQueries
+    {
+        // todo take from config root and subfolder
+        private const string rootFolder = "wwwroot";
+        private const string videoSubFolder = "video";
+
+        public PathInfo GenerateVideoPath(string fileExtension)
+        {
+            var folder = Path.Combine(rootFolder, videoSubFolder);
+            var fileName = new Random().ShortString() + fileExtension;
+            var path = Path.Combine(folder, fileName.ToString());
+
+            return new PathInfo
+            {
+                Path = path,
+                FileName = fileName
+            };
+        }
+
+        public string GetVideoPath(string fileName)
+        {
+            var folder = Path.Combine(rootFolder, videoSubFolder);
+
+            return Path.Combine(folder, fileName);
+        }
+    }
+}
