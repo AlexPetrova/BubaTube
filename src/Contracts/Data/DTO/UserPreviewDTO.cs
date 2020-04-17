ada8ea7827560d9748d192e64df781c9ea4145e1
AlexPetrova <a.petrova95@yahoo.com>
2020-04-17 17:50:41 +0300

    add dto for video and author preview

---
+++ src/Contracts/Data/DTO/UserPreviewDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.Data.DTO
+{
+    public class UserPreviewDTO
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public byte[] ProfilePicture { get; set; }
+    }
+}
---
+++ src/Contracts/Data/DTO/VideoPreviewDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.Data.DTO
+{
+    public class VideoPreviewDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string FileName { get; set; }
+
+        public UserPreviewDTO Author { get; set; }
+    }
+}
