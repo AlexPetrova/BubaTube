081356e07143e1641fc9871ce5b305a2ec2f4bfd
AlexPetrova <a.petrova95@yahoo.com>
2020-04-17 17:54:31 +0300

    add VideoController

---
+++ src/BubaTube/Controllers/VideoController.cs
@@ -0,0 +1,40 @@
+﻿using BubaTube.ViewModels.VideoViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts.Get;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubaTube.Controllers
+{
+    public class VideoController : Controller
+    {
+        private readonly IVideoQueries videoQueries;
+
+        public VideoController(IVideoQueries videoQueries)
+        {
+            this.videoQueries = videoQueries;
+        }
+
+        public IActionResult MostRecent()
+        {
+            IReadOnlyCollection<VideoPreviewViewModel> videos 
+                = this.videoQueries.MostResentVideos()
+                    .Select(dto => new VideoPreviewViewModel
+                    {
+                        Id = dto.Id,
+                        Title = dto.Title,
+                        Url = $"video/{dto.FileName}",
+                        Author= new AuthorPreviewViewModel
+                        {
+                            Id = dto.Author.Id,
+                            FirstName = dto.Author.FirstName,
+                            LastName = dto.Author.LastName,
+                            ProgilePicture = dto.Author.ProfilePicture
+                        }
+                    })
+                    .ToList();
+
+            return base.View(videos);
+        }
+    }
+}
